51f7881f5f538e5c366a4053225a69b2c4ee3106
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-06 23:51:22 -0300

    Testando metodos na classe Usuario

--- src/CatalagoLivro/Usuario.java
+++ src/CatalagoLivro/Usuario.java
@@ -5,15 +5,15 @@ public class Usuario {
     public static void main(String[] args) {
         LivroRepository livroRepository = new LivroRepository();
 
-        livroRepository.adicionarLivro("casa Branca", "machado de assis", 2020);
-        livroRepository.adicionarLivro("casa Branca", "machado de assis", 2020);
-        livroRepository.adicionarLivro("A noite é bela", "Nietzs", 1990);
+        livroRepository.adicionarLivro("Dom Casmurro", "machado de assis", 2020);
+        livroRepository.adicionarLivro("Dom Casmurro", "machado de assis", 2020);
+        livroRepository.adicionarLivro("Assim falou Zaratustra", "nietzsche", 1990);
 
 
         System.out.println(livroRepository.getLivroList());
 
-        livroRepository.removerLivroTitulo("casa Branca");
-
+        livroRepository.removerLivroTitulo("Dom Casmurro");
+        
         System.out.println(livroRepository.getLivroList());
 
 
