a0bc51db64cfc14e933656d2b1c55adb86ace525
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-06 23:48:14 -0300

    Criando metodos do repository

--- src/CatalagoLivro/LivroRepository.java
+++ src/CatalagoLivro/LivroRepository.java
@@ -7,13 +7,67 @@ public class LivroRepository {
 
    private List<Livro> livroList;
 
-    public LivroRepository(List<Livro> livroList) {
+    public LivroRepository() {
         this.livroList = new ArrayList<Livro>();
     }
 
     public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
         this.livroList.add(new Livro(titulo, autor, anoPublicacao));
     }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for (Livro l : livroList) {
+                if(l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAno(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorAno = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorAno.add(l);
+                }
+
+            }
+        }
+        return livrosPorAno;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for (Livro l : livroList) {
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+    public void removerLivroTitulo(String titulo) {
+        List<Livro> livrosRemover = new ArrayList<Livro>();
+        for (Livro l : livroList) {
+            if(l.getTitulo().equalsIgnoreCase(titulo)) {
+                livrosRemover.add(l);
+            }
+        }
+        livroList.removeAll(livrosRemover);
+    }
+
+    public List<Livro> getLivroList() {
+        return livroList;
+    }
+
+
+
 
 
 }
