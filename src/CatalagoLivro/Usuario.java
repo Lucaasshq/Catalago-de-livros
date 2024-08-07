351710289aedc7b7bacd24594fc73fd4a268a3f9
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-07 00:02:24 -0300

    Ultilizando classe usuario para testar metodos

--- src/CatalagoLivro/Usuario.java
+++ src/CatalagoLivro/Usuario.java
@@ -8,13 +8,16 @@ public class Usuario {
         livroRepository.adicionarLivro("Dom Casmurro", "machado de assis", 2020);
         livroRepository.adicionarLivro("Dom Casmurro", "machado de assis", 2020);
         livroRepository.adicionarLivro("Assim falou Zaratustra", "nietzsche", 1990);
+        livroRepository.adicionarLivro("Livro 1", "Autor 1", 2020);
+        livroRepository.adicionarLivro("Livro 1", "Autor 2", 2021);
+        livroRepository.adicionarLivro("Livro 2", "Autor 2", 2022);
+        livroRepository.adicionarLivro("Livro 3", "Autor 3", 2023);
+        livroRepository.adicionarLivro("Livro 4", "Autor 4", 1994);
 
 
-        System.out.println(livroRepository.getLivroList());
-
-        livroRepository.removerLivroTitulo("Dom Casmurro");
-        
-        System.out.println(livroRepository.getLivroList());
+        System.out.println(livroRepository.pesquisarPorAutor("machado de assis"));
+        System.out.println(livroRepository.pesquisarPorTitulo("Assim falou Zaratustra"));
+        System.out.println(livroRepository.pesquisarPorIntervaloAno(2020, 2022));
 
 
 
